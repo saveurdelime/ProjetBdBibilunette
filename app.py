276ea193edda24167d3d6ed9a10f42757d167faa
saveurdelime <lawrencemichaux.lm@gmail.com>
2019-04-04 23:12:42 -0400

    creation des liens entre les pages

--- app.py
+++ app.py
@@ -5,10 +5,41 @@ import requests
 
 app = Flask(__name__)
 @app.route('/')
-def index():
-    return render_template('index.html')
+def acceuil():
+    return render_template('bibilnpageacceuil.html')
+
+@app.route('/bibilnapropos/')
+def bibilnapropos():
+    return render_template('bibilnapropos.html')
+
+@app.route('/bibilncatalogue/')
+def bibilncatalogue():
+    return render_template('bibilncatalogue.html')
+
+@app.route('/bibilnlunettespourlui/')
+def bibilnlunettespourlui():
+    return render_template('bibilnlunettespourlui.html')
+
+@app.route('/bibilnlunettespourelle/')
+def bibilnlunettespourelle():
+    return render_template('bibilnlunettespourelle.html')
+
+@app.route('/bibilnsinscrire/')
+def bibilnsinscrire():
+    return render_template('bibilnsinscrire.html')
+
+
+# <div class = "barredenavigation">
+# <a href="/bibilnpageaccueil">Accueil</a>
+# <a href="/bibilncatalogue">Catalogue</a>
+# <a href="/bibilnlunettespourlui">Lunettes pour lui</a>
+# <a href="/bibilnlunettespourelle">Lunettes pour elle</a>
+# <a href="/bibilnapropos">À propos</a>
+# <a class="liensespaceclient" href="/bibilnseconnecter">Se connecter</a>
+# <a class="liensespaceclient" href="/bibilnsinscrire">S'inscrire</a>
+# </div>
 
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run()
